4e502bd8cdfa9fed3c8e27a7be1c8ae3a833fde4
junkdeck <fred@mslsr.com>
2017-07-20 00:59:55 +0200

    class-based approach. use only for small files as it loads it all into memory at once.

---
+++ main.rb
@@ -0,0 +1,39 @@
+class TextAnalyzer
+
+  def initialize(text)
+    @text = text
+  end
+
+  def get_character_count
+    return @text.split('').length
+  end
+
+  def get_non_whitespace_character_count
+    return @text.scan(/\S/).length
+  end
+
+  def get_line_count
+    return @text.lines.length
+  end
+
+  def get_word_count
+    return @text.split(' ').length
+  end
+
+  def get_sentence_count
+    return @text.split('.').length
+  end
+
+  def get_paragraph_count
+    return @text.split(/\n\n/).length
+  end
+
+  def get_average_words
+    return get_word_count / get_sentence_count
+  end
+
+  def get_average_sentences
+    return get_sentence_count / get_paragraph_count
+  end
+
+end
